a75ee19467a7b7e4a05f263570ab35d128e80a13
Jonathas <jonathas_oliveira17@hotmail.com>
2023-07-19 18:21:35 -0300

    update ex verificar primeiro duplicado
    adicionando algumas validações e os return da função

--- curso luiz otavio/exercicios/ex20.py
+++ curso luiz otavio/exercicios/ex20.py
@@ -1,30 +1,51 @@
 import os
+
 lista_de_inteiros = []
 
+
+
 while True:
-    numero_inteiro = int(input('Digite um número inteiro [999 para encerrar]: '))
+    try:
+        numero_inteiro = int(input('Digite um número inteiro [999 para encerrar]: '))
+
+    except ValueError:
+        os.system('cls')
+        print('Valor inválido. Por favor, digite apenas números inteiros.')
 
     if numero_inteiro == 999:
         break
+
     lista_de_inteiros.append(numero_inteiro)
     os.system('cls')
+
+
 
 numero_set = set()
 
 def duplicado(lista):
     global numero_set
-    retornar = 0
+    first_repeat = 0
     verificar = 0
 
     for numero in lista_de_inteiros:
+
         if verificar == 0:
+
             if numero in numero_set:
-                retornar = numero
+                first_repeat = numero
                 verificar += 1
+                return first_repeat
+
         numero_set.add(numero)
 
-    return retornar
+    return -1
+
 
 primeiro_numero_a_duplicar = duplicado(1)
 
-print(f'{numero_set}\nPrimeiro número duplicado: {primeiro_numero_a_duplicar}')
+
+if primeiro_numero_a_duplicar == -1:
+    print(f'{numero_set}\nNão há números duplicados')
+
+else:
+    print(f'{numero_set}\nPrimeiro número duplicado: {primeiro_numero_a_duplicar}')
