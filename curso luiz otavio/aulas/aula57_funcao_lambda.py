ce969856485fd0e60b3c8b07c67a8015831c661e
Jonathas <jonathas_oliveira17@hotmail.com>
2023-07-24 15:52:52 -0300

    Função Lambda

---
+++ curso luiz otavio/aulas/aula57_funcao_lambda.py
@@ -0,0 +1,30 @@
+# Função lambd em Python
+# A função lambda é uma função como qualquer outra.
+# Porém, são funções anônimas que contém apenas uma linha. 
+# Ou seja, tudo deve ser contido dentro de uma única expressão
+
+lista = [
+    {'nome': 'Jonathas', 'sobrenome': 'Barros'},
+    {'nome': 'Braia', 'sobrenome': 'Vinicius'},
+    {'nome': 'Micha', 'sobrenome': 'Jhonson'},
+    {'nome': 'Luiz', 'sobrenome': 'Dragneel'},
+]
+
+
+# def ordenar(item):
+#     return item['sobrenome']
+
+# lista.sort(key=ordenar)
+
+# for item in lista:
+#     print('item')
+
+# ordena em ordem alfabética com base no sobrenome
+# o return para ordenar o dicionário. Nesse caso, 
+# como é ['sobrenome'], será ordenado pelo sobrenome.
+
+
+lista.sort(key=lambda item: item['nome'])
+
+for item in lista:
+    print(item)
