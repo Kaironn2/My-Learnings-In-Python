765181654dfcefd73275c582362d402f00ba3093
Jonathas <jonathas_oliveira17@hotmail.com>
2023-07-27 14:47:53 -0300

    Verificar antecessor e euscessor

--- curso em video/desafios/desafio_005.py
+++ curso em video/desafios/desafio_005.py
@@ -1,7 +1,14 @@
 import os
 
+def vizinhos(numero):
+    antecessor = numero - 1
+    sucessor = numero + 1
+    return antecessor, sucessor
+
 while True:
+
     while True:
+
         try:
             numero = int(input('Digite um número para ver seu sucessor e seu atencessor: '))
             break
@@ -9,24 +16,34 @@ while True:
             os.system('cls')
             print('Valor inválido. Por favor, digite apenas números.')
 
-    def vizinhos(numero):
-        antecessor = numero - 1
-        sucessor = numero + 1
+    os.system('cls')
 
-        os.system('cls')
+    antecessor, sucessor = vizinhos(numero)
 
-        print(
-            f'Antecessor -> {antecessor}\n'
-            f'número -----> {numero}\n'
-            f'Sucessor ---> {sucessor}'
-            )
+    print(
+        f'Antecessor -> {antecessor}\n'
+        f'número -----> {numero}\n'
+        f'Sucessor ---> {sucessor}'
+        )
 
-    vizinhos(numero)
+    while True:
 
-    opcao = input('Você deseja analisar outro número? [s]im [n]ão: ').upper().startswith('S')
+        opcao = input('Você deseja realizar outra operação? [s]im [n]ão: ').upper()
 
-    if opcao:
-        os.system('cls')
-    else:
-        print('Programa finalizado.')
+        if opcao == '':
+            os.system('cls')
+            continue
+        elif opcao[0] in 'S':
+            os.system('cls')
+            encerrar = False
+            break
+        elif opcao[0] in 'N':
+            encerrar = True
+            break
+        else:
+            os.system('cls')
+            print('Opção inválida. Por favor, digite SIM para continuar e NÃO para finalizar.')
+    
+    if encerrar:
+        print('Programa encerrado')
         break
