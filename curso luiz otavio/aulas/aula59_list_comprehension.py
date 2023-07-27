32f7f44ebba8f3887614916332a8e33b5fdf1f83
Jonathas <jonathas_oliveira17@hotmail.com>
2023-07-27 19:49:03 -0300

    List comprehension
    Introdução

---
+++ curso luiz otavio/aulas/aula59_list_comprehension.py
@@ -0,0 +1,9 @@
+# List Comprehension é uma forma rápida parar criar listas
+# a partirde iteráveis.
+
+# Devemos digitar à esquerda do for o que queremos incluir na lista
+# Também é permitido fazer operações
+# e usar a variável do for, como no exemplo: 
+lista = [numero * 2 for numero in range(1, 11)]
+
+print(lista)
