0f8e7c9f168f54d02f1112bf95a82a4f41b5df61
Jonathas <jonathas_oliveira17@hotmail.com>
2023-07-28 03:27:18 -0300

    List Comprehension
    Mapeamento de dados com list comprehension

--- curso luiz otavio/aulas/aula59_list_comprehension.py
+++ curso luiz otavio/aulas/aula59_list_comprehension.py
@@ -7,3 +7,27 @@
 lista = [numero * 2 for numero in range(1, 11)]
 
 print(lista)
+
+
+# Mapeamento de dados em list comprehension
+
+produtos = [
+
+    {'nome': 'papel', 'preço': 10},
+    {'nome': 'agua', 'preço': 15},
+    {'nome': 'vaso', 'preço': 20}
+
+]
+
+# Vamos fazer um exemplo para aumentar o valor de cada produto em 5%
+# E vamos mudar as chaves de 'nome' para 'produto' e 'preço' para 'valor'
+# Pode ser feito em apenas uma linha ou também do modo a seguir: 
+# novos_produtos = [{'produto': produto['nome'], 'valor': produto['preço'] * 1.05} if produto['preço'] >= 15 else {**produto} for produto in produtos]
+
+novos_produtos = [
+    {'produto': produto['nome'], 'valor': produto['preço'] * 1.05} 
+                  if produto['preço'] >= 15 else {**produto} 
+                  for produto in produtos
+                  ]
+
+print(*novos_produtos, sep='\n')
