5b30bf3662b20e06206c6884fc2eae7e17b8755c
Jonathas <jonathas_oliveira17@hotmail.com>
2023-08-03 00:56:06 -0300

    Calculadora de Catetos/Hipotenusa
    Menu de opções feito.
    Falta fazer a lógica do cálculo.

--- curso em video/desafios/desafio_017.py
+++ curso em video/desafios/desafio_017.py
@@ -1,7 +1,8 @@
 import os 
 
-ca_co_hi = {}
+ca_co_hi = {'Cateto Oposto': 0, 'Cateto Adjacente': 0, 'Hipotenusa': 0}
 contador = 0
+lista_opcoes = []
 
 def validar_float(mensagem):
     while True:
@@ -12,21 +13,74 @@ def validar_float(mensagem):
             os.system('cls')
             print('Valor inválido. Por favor, digite apenas números. Use o . no lugar na vírgula.')
 
-def redefinir_lados(ok):
+def redefinir_lados():
     global ca_co_hi
-    ca_co_hi = {'cateto_oposto': 'Cateto Oposto', 'cateto_adjacente': 'Cateto Adjacente', 'hipotenusa': 'Hipotenusa'}
+    ca_co_hi = {'Cateto Oposto': 0, 'Cateto Adjacente': 0, 'Hipotenusa': 0}
 
 def redefinir_contador(valor_contador):
     global contador
     contador = valor_contador
 
-while True:
+def opcoes_inseridas():
+        pular_linha = None
+        for chave, valor in ca_co_hi.items():
+            if valor > 0:
+                print(f'{chave}: {valor}')
+                pular_linha = True
+        if pular_linha:
+            print()
 
-    redefinir_lados(1)
+def mostrar_menu():
+    global contador
+    global lista_opcoes
 
     while True:
 
-        for chave in ca_co_hi:
-            print(chave)
+        opcoes_inseridas()
+        redefinir_contador(1)
+
+        for chave in ca_co_hi.keys():
+            print(f'{contador} - {chave}')
+            contador += 1
+        print(f'{contador} - Sair')
         
-        opcao = input('Qual opção você deseja inserir? ')
+        opcao = input('\nInsira uma opção: ')
+
+        if opcao == '':
+            os.system('cls')
+            print('Você não digitou nada. Digite uma das opções listadas')
+        elif len(opcao) > 1:
+            os.system('cls')
+            print('Digite apenas uma opção.')
+        elif opcao in '123':
+            if opcao not in lista_opcoes:
+                lista_opcoes.append(opcao)
+                return opcao[0]
+            else:
+                os.system('cls')
+                print('Essa opção já foi inserida. Digite outra opção.')
+        else:
+            os.system('cls')
+            print('Opção inválida. Por favor, digite uma das opções listadas.')
+
+
+while True:
+
+    redefinir_lados()
+
+    while True:
+
+        opcao = mostrar_menu()
+
+        os.system('cls')
+
+        match opcao:
+
+            case '1':
+                ca_co_hi['Cateto Oposto'] = validar_float('Insira o valor do cateto oposto: ')
+            case '2':
+                ca_co_hi['Cateto Adjacente'] = validar_float('Insira o valor do cateto adjacente: ')
+            case '3':
+                ca_co_hi['Hipotenusa'] = validar_float('Insira o valor do hipotenusa: ')
+
+        os.system('cls')
