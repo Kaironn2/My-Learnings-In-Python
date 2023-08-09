469272007b49ecaeb8037eeeba581a78dfdc1e41
Jonathas <jonathas_oliveira17@hotmail.com>
2023-08-08 21:12:24 -0300

    Funções Decoradoras e Decorators
    Revisando o assunto e adicionando comentários para melhor entendimento.

--- curso luiz otavio/aulas/aula71_funcoes_decoradoras.py
+++ curso luiz otavio/aulas/aula71_funcoes_decoradoras.py
@@ -1,23 +1,25 @@
 import time
 
-def medir_tempo(funcao):
-    def decorator(*args, **kwargs):
-        inicio = time.time()                # antes da execução
-        resultado = funcao(*args, **kwargs) # execução
-        fim = time.time()                   # depois da execução
+def medir_tempo(funcao):                # o decorator irá ter como parâmetro a função que será executada
+
+    def wrapper(x, y):                  # o wrapper é definido logo após o decorator e terá nos parâmetros os parâmetros da função que será executada
+                                        # que nesse caso é a função somar. Mas poderíamos passar *args, **kwargs para receber parâmetros variáveis de várias funções
+
+        inicio = time.time()            # aqui decidimos o que será executado antes da função
+
+        retornar = funcao(x, y)         # aqui chamamos o parâmetro função para executar a função que será passada
+
+        fim = time.time()               # aqui decidimos o que faremos depois da execução da função
         tempo_passado = fim - inicio
-        print(f'A função {funcao.__name__} levou {tempo_passado:.2f} segundos para ser executada.')
-        return resultado
-    return decorator
+        print(f'O tempo decorrido da função "{funcao.__name__}" foram {tempo_passado:.2f} segundos.')
 
-@medir_tempo                                # decorator
-def inverte_string(*args):
-    for arg in args:
-        if not isinstance(arg, str):
-            raise ValueError(f"{arg} não é um string")
+        return retornar                 # Aqui, caso tenhamos guardado o return da função em um variável, retornaremos a variável
     
-    time.sleep(2)  # Simulando uma operação demorada
-    return [arg[::-1] for arg in args]
+    return wrapper                      # e, por fim, retornaremos o wrapper
 
-resultado = inverte_string("abc")
-print('Resultado:', resultado)
+@medir_tempo                            # o decorator deve vir com um @ seguido de seu nome na linha anterior à definição da função
+def soma(x, y):
+    time.sleep(1)
+    return x + y
+
+somar = soma(3, 4)
