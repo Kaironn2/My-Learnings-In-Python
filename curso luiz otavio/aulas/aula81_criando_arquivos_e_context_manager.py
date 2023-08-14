d7ea8aaddc4e27f6b4fb2aa12eafff2a5edb1865
Jonathas <jonathas_oliveira17@hotmail.com>
2023-08-14 15:35:31 -0300

    Criando Arquivos + Context Manager With + Caminhos
    Criando, escrevendo e lendo arquivos
    Caminho e instruções

---
+++ curso luiz otavio/aulas/aula81_criando_arquivos_e_context_manager.py
@@ -0,0 +1,55 @@
+# Criando arquivos com Python + Context Manager with
+# Usamos a função open para abrir
+# um arquivo em Python (ele pode ou não existir)
+# Modos:
+# r (leitura), w (escrita), x (para criação)
+# a (escreve ao final), b (binário)
+# t (modo texto), + (leitura e escrita)
+# Context manager - with (abre e fecha)
+# Métodos úteis
+# write, read (escrever e ler)
+# writelines (escrever várias linhas)
+# seek (move o cursor)
+# readline (ler linha)
+# readlines (ler linhas)
+# Vamos falar mais sobre o módulo os, mas:
+# os.remove ou unlink - apaga o arquivo
+# os.rename - troca o nome ou move o arquivo
+# Vamos falar mais sobre o módulo json, mas:
+# json.dump = Gera um arquivo json
+# json.load
+
+caminho_arquivo = 'C:\\Users\\rosan\\Desktop\\Python\\Outros Arquivos\\aula81.txt'
+
+# arquivo = open(caminho_arquivo, 'w') # No windows, invés apenas uma \, use \\ para separar o caminho
+# Caso o caminho no windows não funcione, teste botar um r na frente. r'C:\\Pasta\\arquivo\\'
+# #
+# arquivo.close()
+with open(caminho_arquivo, 'w') as arquivo:
+    print('Olá mundo')
+    print('Arquivo vai ser fechado')
+
+with open(caminho_arquivo, 'w+') as arquivo:
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(
+        ('Linha 3\n', 'Linha 4\n')
+    )
+    arquivo.seek(0, 0)
+    print(arquivo.read())
+    print('Lendo')
+    arquivo.seek(0, 0)
+    print(arquivo.readline(), end='')
+    print(arquivo.readline().strip())
+    print(arquivo.readline().strip())
+
+    print('READLINES')
+    arquivo.seek(0, 0)
+    for linha in arquivo.readlines():
+        print(linha.strip())
+
+
+print('#' * 10)
+
+with open(caminho_arquivo, 'r') as arquivo:
+    print(arquivo.read())
