deb040e2799155855e10f780adc34b6797863efa
Jonathas <jonathas_oliveira17@hotmail.com>
2023-08-14 14:28:09 -0300

    Requirements.txt, Pacotes em venv

---
+++ curso luiz otavio/aulas/aula80_requirementstxt.py
@@ -0,0 +1,8 @@
+# Mostrar pacotes instalados:
+# pip freeze
+
+# Gerar arquivo txt requirements (O nome do arquivo pode ser qualquer outro):
+# pip freeze > requirements.txt
+
+# Instalar os pacotes do requirements:
+# pip install -r ./requirements.txt
