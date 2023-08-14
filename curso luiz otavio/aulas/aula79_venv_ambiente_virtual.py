8240eed6a3d8094855675c6539d28f1fb0b7ce37
Jonathas <jonathas_oliveira17@hotmail.com>
2023-08-14 11:50:41 -0300

    Ambiente Virtual(venv) e Pip

--- curso luiz otavio/aulas/aula79_venv_ambiente_virtual.py
+++ curso luiz otavio/aulas/aula79_venv_ambiente_virtual.py
@@ -8,3 +8,21 @@
 # Você pode dar o nome que preferir para um
 # ambiente virtual, mas os mais comuns são:
 # venv env .venv .env
+
+# Para criar um ambiente virtual no windows, no terminal insira a linha de código:
+# python -m venv nomedoambiente
+# Geralmente, usamos "venv" ou "env" como nome do ambiente virtual
+
+# para ver o path do python, no powershell use:
+# gcm python ou gcm python -Syntax
+
+# Para ativar o ambiente virtual, digite o caminho do activate
+# venv/Scripts/activate
+# E para desativar o ambiente virtual, digite deactivate
+
+# Para instalar pacotes:
+# pip install nomedopacote
+# Para desinstalar pacotes:
+# pip install nomedopacote
+# Para listar versões do pacote:
+# pip index versions pymysql
