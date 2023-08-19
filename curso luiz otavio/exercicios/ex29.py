cc5f566565d1f28b1ce14a370f4d5a2204682652
Jonathas <jonathas_oliveira17@hotmail.com>
2023-08-19 03:42:55 -0300

    Car Specs com POO
    Vendo especificações de carros com classes

--- curso luiz otavio/exercicios/ex29.py
+++ curso luiz otavio/exercicios/ex29.py
@@ -8,29 +8,31 @@
 # Obs.: Um fabricante pode fabricar vários carros
 # Exiba o nome do carro, motor e fabricante na tela
 
+import os
+
+
 class Company:
     def __init__(self, company):
         self.company = company
-        self.car = None
-        self.engine = None
+        self.cars = {}
 
-    def set_car(self, name):
-        self.car = Car(name)
+    def add_car(self, car_name, engine_name):
+        car = Car(car_name, engine_name)
+        self.cars[car_name] = car
 
-    def set_engine(self, name):
-        self.engine = Engine(name)
+    def find_car(self, car_name):
+        return self.cars.get(car_name, None)
 
-    def car_spec(self):
-        print(
-            f'Fabricante: {self.company}\n'
-            f'Carro: {self.car.name}\n'
-            f'Motor: {self.engine.name}\n'
-        )
+    def company_cars(self):
+        print(f'Fabricante: {self.company}')
+        for car_name, car in self.cars.items():
+            print(car.name)
 
 
 class Car:
-    def __init__(self, name):
+    def __init__(self, name, engine_name):
         self.name = name
+        self.engine = Engine(engine_name)
 
 
 class Engine:
@@ -39,6 +41,28 @@ class Engine:
 
 
 ford = Company('Ford')
-ford.set_engine('Duratec 2.0')
-ford.set_car('Focus')
-ford.car_spec()
+ford.add_car('Focus', 'Duratec 2.0')
+ford.add_car('Celta', 'Partial 1.8')
+
+
+while True:
+
+    print('-' * 20)
+    ford.company_cars()
+    print('-' * 20)
+
+    user_find_car = input(
+        '\nDigite o nome do carro que você deseja ver as especificações: ').capitalize()
+    if user_find_car == 'Sair':
+        print('Programa encerrado.')
+        break
+    user_car = ford.find_car(user_find_car)
+
+    os.system('cls')
+
+    if user_car:
+        print(f'Especificações do {user_find_car}:')
+        print(f'Carro: {user_car.name}')
+        print(f'Motor: {user_car.engine.name}')
+    else:
+        print(f'Carro {user_find_car} não encontrado.')
