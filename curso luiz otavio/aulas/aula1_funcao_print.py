3fc75e2103807f70244cdcf88e21265755461c08
Jonathas <jonathas_oliveira17@hotmail.com>
2023-08-23 13:17:37 -0300

    Função Print - Docstring - Comentários
    Nessa aula aprendemos como mostrar algo na tela usando a função print, criar comentários e DocStrings em python, que servem como anotações.

--- curso luiz otavio/aulas/aula1_funcao_print.py
+++
@@ -1,8 +0,0 @@
-#Os comentários são escritos com uma # no início.
-
-print('braia k k k') #<-- exibir na tela
-
-#aspas triplas é DocString
-"""
-DocString
-"""
---
+++ curso luiz otavio/aulas/aula1_funcao_print_e_docstring.py
@@ -0,0 +1,27 @@
+# Para fazer comentários em python, use # na linha de código.
+# O comentário começará a partir da #.
+# Então, na linha de código que você usar uma #, nada após ela será executado.
+
+
+# Por exemplo, nessa linha, só será executado até print('braia k k k')
+print('braia k k k')
+
+# A função print serve para mostrar algo na tela
+# O que será mostrado na tela, estará dentro de aspas e do nosso texto, como no exemplo abaixo.
+print('Minha mensagem')
+# Nesse caso, o que será mostrado na tela será "Minha mensagem"
+
+# Caso a gente queira usar aspas dentro da string, usaremos aspas compostas. Da seguinte forma:
+# As aspas simples usaremos apenas para definir as strings
+# Strings são basicamente textos
+print('Essa é a minha "mensagem"')
+
+
+# Aspas triplas em python servem para criar DocStrings, que também servem como um comentário,
+# só que diferente da #, o comentário só termina quando as aspas triplas são fechadas.
+# Ex:
+"""
+linha 1
+linha 2
+linha 3
+"""
