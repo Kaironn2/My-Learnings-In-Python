036921624dd758089e3798f3b8f067885ca0cf64
Jonathas <jonathas_oliveira17@hotmail.com>
2023-08-26 01:39:00 -0300

    if __name__ == '__main__'
    Nessa aula vimos a função da condição __name__ == '__main__' que serve para executar linhas de código apenas se o módulo for executado diretamente e não executar caso seja importado.
    
    In this lesson, we learned about the use of __name__ == '__main__', which is used to execute lines of code only if the module is run directly and not executed if its imported.

---
+++ curso luiz otavio/aula_modulos/aula1_if_name_main.py
@@ -0,0 +1,8 @@
+# if __name__ == '__main__' serve para verificar se o módulo que está sendo
+# executado é o módulo principal. Caso seja, ele irá executar as linhas de
+# código dentro da condição. Em resumo, serve para caso você importe um módulo,
+# importe apenas o que você. Tudo que você não deseja importar, deverá ser
+# posto dentro do if __name__
+
+if __name__ == '__main__':
+    print('ok')
