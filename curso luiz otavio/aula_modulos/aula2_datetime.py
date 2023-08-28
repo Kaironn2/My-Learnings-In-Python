dd6675b1bd52d1a27718724864ce6a568dc6ae5d
Jonathas <jonathas_oliveira17@hotmail.com>
2023-08-28 15:02:43 -0300

    datetime module
    Nessa aula, nós aprendemos sobre o módulo datetime. Com ele, podemos criar datas, trabalhar com diferentes timezones com o pacote pytz, entre outras coisas.
    
    In this lesson, we learned about the datetime module. With it, we can create dates, work with differents time zones using the pytz package, and other things.

--- curso luiz otavio/aula_modulos/aula2_datetime.py
+++ curso luiz otavio/aula_modulos/aula2_datetime.py
@@ -12,10 +12,34 @@
 # pip install pytz types-pytz
 from datetime import datetime
 
+from pytz import timezone
+
+#               ano/mes/dia hr/min/sec
 data_str_data = '2022/04/20 07:49:23'
-data_str_data = '20/04/2022'
+# data_str_data = '20/04/2022'
 data_str_fmt = '%d/%m/%Y'
 
-# data = datetime(2022, 4, 20, 7, 49, 23)
+# data = datetime(2022, 4, 20, 7, 49, 23, tzinfo=timezone('America/Bahia'))
 data = datetime.strptime(data_str_data, data_str_fmt)
 print(data)
+
+
+# Horário atual do computador
+print('*' * 50)
+print('Horário atual do computador')
+print(datetime.now())
+
+# Horário atual timezone específica
+print('*' * 50)
+print('Horário atual, timezone específica: ')
+print(datetime.now(timezone('America/Bahia')))
+
+# Número de segundos - Era Unix - Podemos recriar datas usando esses segundos
+print('*' * 50)
+print('Recriando data a partir de segundos Era Unix: ')
+print(datetime.fromtimestamp(1650423600.0))
+
+# Para pegar o Unix Timestamp de uma data:
+print('*' * 50)
+print('Recriando data a partir de segundos Era Unix: ')
+print(data.timestamp())
