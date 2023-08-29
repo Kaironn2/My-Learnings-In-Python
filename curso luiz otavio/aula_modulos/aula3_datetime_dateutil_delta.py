c915f73fde8d410b5c4272dc77aaa7c8a7974857
Jonathas <jonathas_oliveira17@hotmail.com>
2023-08-29 11:47:52 -0300

    deltatime with dateutil and datetime
    Nessa aula, nós aprendemos sobre o delta time(diferença de datas). Nós calculamos o delta time de diferentes formas usando o módulo datetime e o pacote dateutil.
    
    In this lesson, we learned about the delta time(date difference). We calculate in differents ways using the datetime module and dateutil package.

--- curso luiz otavio/aula_modulos/aula3_datetime_dateutil_delta.py
+++ curso luiz otavio/aula_modulos/aula3_datetime_dateutil_delta.py
@@ -3,10 +3,11 @@ from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 fmt = '%d/%m/%Y %H:%M:%S'
-start_date = datetime.strptime('28/08/2023 15:04:36', fmt)
+start_date = datetime.strptime('28/06/2023 15:04:36', fmt)
 end_date = datetime.strptime('28/08/2023 16:00:00', fmt)
 delta_dates = end_date - start_date  # delta de duas datas
 delta = timedelta(days=10, hours=5)  # criando um timedelta
+deltadateutil_ = relativedelta(start_date, end_date)
 
 # Outra opção para trabalhar com deltatime. Diferente do timedelta do datetime,
 # o relativatedelta tem mais opções.
@@ -28,4 +29,9 @@ print('*' * 50)
 print('Diferença total em segundos: ', delta.total_seconds())
 
 # Diferença entre uma data e um timedelta:
+print('*' * 50)
 print('Diferença entre uma data e um timedelta: ', end_date - delta)
+
+# Vendo diferenças de data de uma forma mais completa com o relativedelta
+print('*' * 50)
+print(deltadateutil_)
