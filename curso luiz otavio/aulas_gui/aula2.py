a174ccd5dfd7c66d7e47863693f8350b9d5b858e
Jonathas <jonathas_oliveira17@hotmail.com>
2023-09-12 11:30:50 -0300

    testing pyside6
    Nessa aula, nós apenas testamos a instalação do PySide6 e mostramos sua versão.
    
    In this lesson, we just tested the pyside6 installation and displayed its version.

---
+++ curso luiz otavio/aulas_gui/aula2.py
@@ -0,0 +1,7 @@
+import PySide6.QtCore
+
+# Prints PySide6 version
+print('Pyside6 version:', PySide6.__version__)  # type: ignore
+
+# Prints the Qt version used to compile PySide6
+print('Qt version:', PySide6.QtCore.__version__)  # type: ignore
